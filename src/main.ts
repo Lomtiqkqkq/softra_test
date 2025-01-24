79b1558a534906f1acc7fb992266c47cdcb2ca10
Paul <mokrenko51@gmail.com>
2025-01-25 00:04:42 +0300

    create global exception

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { HttpExceptionFilter } from './exception/global.exception.filter';
 
 const configService = new ConfigService();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = configService.get<string>('PORT') || 3000;
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(port, () => {
     console.log(`listen: ${port}`);
   });
